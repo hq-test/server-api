f4e721b1ad63f407854274ace2ccdf35721be81a
Ali Pour Zahmatkesh <mr.bardia@gmail.com>
2018-05-26 04:24:56 +0430

    update archive of auctions to show all archives, not dependent to logged in user

--- api/controllers/auction/readarchived.js
+++ api/controllers/auction/readarchived.js
@@ -7,31 +7,17 @@ module.exports = async function readArchived(req, res) {
   }
 
   try {
-    var socketId = sails.sockets.getId(req);
-    var partner = await Partner.findOne({ socket: socketId });
-
-    var auctions = [];
-    var myAuctions = await sails.sendNativeQuery(
-      `select auction from bid where partner = $1 group by auction`,
-      [partner.id]
-    );
-
-    if (myAuctions && myAuctions.rows) {
-      var myAuctionsId = _.pluck(myAuctions.rows, 'auction');
-
-      auctions = await Auction.find({
-        where: {
-          id: myAuctionsId,
-          isRunning: false,
-          isActive: true,
-          startAt: { '>': 0 },
-          endAt: { '>': 0 }
-        },
-        sort: 'endAt ASC'
-      })
-        .populate('room')
-        .populate('bids', { limit: 1, sort: 'createdAt DESC' });
-    }
+    var auctions = await Auction.find({
+      where: {
+        isRunning: false,
+        isActive: true,
+        startAt: { '>': 0 },
+        endAt: { '>': 0 }
+      },
+      sort: 'endAt ASC'
+    })
+      .populate('room')
+      .populate('bids', { limit: 1, sort: 'createdAt DESC' });
 
     return res.json({ result: true, data: auctions });
   } catch (err) {
